4bb3d49a3d26231e61e01fe0a303859ce57b1e37
Faialharthi <faialialharthi.d@gmail.com>
2024-09-26 16:40:42 +0300

    Initial Commit

---
+++ testwithGit/testwithGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testwithGitApp.swift
+//  testwithGit
+//
+//  Created by Fai Alharthi on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct testwithGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
